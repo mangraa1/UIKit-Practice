9d2be4c62f039fe9ee088d0d7602c795804fd47a
mangraa1 <georgiu.spare@gmail.com>
2023-05-12 11:03:20 +0300

    UISlider practic(fixed)
    interest rate calculation

---
+++ UISlider practic/UISlider/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  UISlider
+//
+//  Created by mac on 20.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var commonAmountLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var periodLabel: UILabel!
+    @IBOutlet weak var percentLabel: UILabel!
+    
+    @IBOutlet weak var amountSlider: UISlider!
+    @IBOutlet weak var periodSlider: UISlider!
+    @IBOutlet weak var percentSlide: UISlider!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        amountLabel.text = "10 000 грн."
+        periodLabel.text = "5 місяців"
+        percentLabel.text = "5 %"
+        setCommonAmount()
+        
+    }
+    
+    
+    @IBAction func amountSliderAction(_ sender: UISlider) {
+        
+        switch sender.tag{
+        case 0: amountLabel.text = "\(Int(sender.value)) грн."
+        case 1: periodLabel.text = "\(Int(sender.value)) місяців"
+        case 2: percentLabel.text = "\(Int(sender.value)) %"
+        default: return
+        }
+        setCommonAmount()
+    }
+    
+    func setCommonAmount(){
+        let percentAmount = amountSlider.value / 100 * percentSlide.value / 12
+        let periodAmount = percentAmount * periodSlider.value
+        commonAmountLabel.text = "\(amountSlider.value + periodAmount) грн."
+    }
+    
+}
+
