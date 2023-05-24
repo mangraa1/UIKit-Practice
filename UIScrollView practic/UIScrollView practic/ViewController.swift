b9e4f5c5e023b59a02be7e0e966414a0da61a03b
mangraa1 <georgiu.spare@gmail.com>
2023-05-24 13:38:16 +0300

    added user agreement screen

--- UIScrollView practic/UIScrollView practic/ViewController.swift
+++ UIScrollView practic/UIScrollView practic/ViewController.swift
@@ -10,49 +10,66 @@ import UIKit
 class ViewController: UIViewController, UIScrollViewDelegate {
     
     var scrollView = UIScrollView()
-    var imageView = UIImageView()
+    var label = UILabel()
+    var button = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //MARK: - ScrollView settings
-
-        scrollView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
-        scrollView.center = view.center
+        
         view.addSubview(scrollView)
-
-        scrollView.layer.borderWidth = 1
-        scrollView.layer.borderColor = UIColor.black.cgColor
-
-        scrollView.contentSize = CGSize(width: 600, height: 600)
-        imageView = UIImageView(image: UIImage(systemName: "pencil.circle.fill"))
-        scrollView.addSubview(imageView)
-        imageView.frame.size = scrollView.contentSize
+        scrollView.addSubview(label)
+        scrollView.addSubview(button)
         
         scrollView.delegate = self
-        scrollView.minimumZoomScale = 0.5
-        scrollView.maximumZoomScale = 2
         
-//        scrollView.contentInset = UIEdgeInsets(top: -150, left: 15, bottom: 15, right: 15)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 23)
+        label.text = "A Terms and Conditions agreement acts as a legal contract between you (the company) and the user. It's where you maintain your rights to exclude users from your app in the event that they abuse your website/app, set out the rules for using your service and note other important details and disclaimers. Having a Terms and Conditions agreement is completely optional. No laws require you to have one. Not even the super-strict and wide-reaching General Data Protection Regulation (GDPR). Your Terms and Conditions agreement will be uniquely yours. While some clauses are standard and commonly seen in pretty much every Terms and Conditions agreement, it's up to you to set the rules and guidelines that the user must agree to. A Terms and Conditions agreement acts as a legal contract between you (the company) and the user. It's where you maintain your rights to exclude users from your app in the event that they abuse your website/app, set out the rules for using your service and note other important details and disclaimers. Having a Terms and Conditions agreement is completely optional. No laws require you to have one. Not even the super-strict and wide-reaching General Data Protection Regulation (GDPR). Your Terms and Conditions agreement will be uniquely yours. While some clauses are standard and commonly seen in pretty much every Terms and Conditions agreement, it's up to you to set the rules and guidelines that the user must agree to."
         
-//        scrollView.isDirectionalLockEnabled = true
-//
-//        scrollView.showsVerticalScrollIndicator = false
-//        scrollView.showsHorizontalScrollIndicator = false
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.red.cgColor
+        button.setTitle("Ok", for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
         
+        //MARK: - Constraints
+        
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        
+            label.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            label.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            label.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 16),
+            button.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            button.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            button.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -16)])
         
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-//        sleep(1)
-//        scrollView.setContentOffset(CGPoint(x: 150, y: 150), animated: true)
+    //MARK: - Actions
+    
+    @objc func buttonAction() {
+        print("the user has confirmed the user agreement")
     }
 
     //MARK: - delegate
     
-    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return imageView
+    func scrollViewDidScrollToTop(_ scrollView: UIScrollView) {
+        print("user scrolled to top")
     }
+    
 
 }
 
