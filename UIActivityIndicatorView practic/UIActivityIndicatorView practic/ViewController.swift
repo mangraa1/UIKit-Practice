90ebfa176b6531b6ad66050d7e23a579a12e16ec
mangraa1 <georgiu.spare@gmail.com>
2023-05-27 15:23:55 +0300

    little practice with progressView

--- UIActivityIndicatorView practic/UIActivityIndicatorView practic/ViewController.swift
+++ UIActivityIndicatorView practic/UIActivityIndicatorView practic/ViewController.swift
@@ -9,35 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var activityIndicatorView = UIActivityIndicatorView()
+    let progressView = UIProgressView(frame: CGRect(x: 0, y: 0, width: 300, height: 10))
+    let progress = Progress(totalUnitCount: 11)
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //MARK: - activityIndicatorView
+        //MARK: - progressView
 
-        view.addSubview(activityIndicatorView)
-        activityIndicatorView.center = view.center
-        activityIndicatorView.style = .large
+        view.addSubview(progressView)
 
-        activityIndicatorView.color = .red
-        activityIndicatorView.startAnimating()
-        activityIndicatorView.hidesWhenStopped = true
+        progressView.center = view.center
+        progressView.progress = 0.3
+        progressView.progressTintColor = .systemBlue
+        progressView.trackTintColor = .yellow
 
+        progressView.observedProgress = progress
+
+        progress.cancellationHandler = {
+            print("progress was stopped")
+        }
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
-            self.activityIndicatorView.stopAnimating()
+        var count: Int64 = 0
+        Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true) { timer in
 
-            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
-                self.activityIndicatorView.startAnimating()
+            if self.progress.isCancelled {
+                timer.invalidate()
             }
+
+            if self.progress.fractionCompleted == 1 {
+                self.progress.cancel()
+            }
+
+            self.progress.completedUnitCount = count
+            count += 1
         }
     }
-
 
 }
 
