9aa84eb705e5b18d9b54c0895680d82ec32a7b97
mangraa1 <georgiu.spare@gmail.com>
2023-06-01 19:18:40 +0300

    added "Source" and "ContactCell"

---
+++ UITableView practic/UITableView practic/Source.swift
@@ -0,0 +1,60 @@
+//
+//  Source.swift
+//  UITableView practic
+//
+//  Created by mac on 01.06.2023.
+//
+
+import Foundation
+import UIKit
+
+struct Contact {
+    let name: String
+    let image: UIImage
+    let description: String
+    let gender: Gender
+}
+
+enum Gender {
+    case male
+    case female
+}
+
+struct ImageName {
+    static let person = "person"
+    static let fillPerson = "person.fill"
+}
+
+struct Description {
+    static let descriptionMale = "friendly man"
+    static let descriptionFemale = "friendly women"
+}
+
+struct Source {
+    static func makeContacts() -> [Contact] {
+        let femaleNames = ["Anna", "Isla", "Luna", "Iris", "Katrin", "Sol", "Amiri", "Carla"]
+        let maleNames = ["William", "Lucas", "Kayce", "Zen", "Eren", "Jrue"]
+        var contacts = [Contact]()
+
+        for name in femaleNames {
+            let newContact = Contact(name: name, image: .init(systemName: ImageName.person)!, description: Description.descriptionFemale, gender: Gender.female)
+            contacts.append(newContact)
+        }
+
+        for name in maleNames {
+            let newContact = Contact(name: name, image: .init(systemName: ImageName.fillPerson)!, description: Description.descriptionMale, gender: Gender.male)
+            contacts.append(newContact)
+        }
+ 
+        return contacts
+    }
+
+    static func sortingContactsByGender() -> [[Contact]] {
+        let male = makeContacts().filter { $0.gender == .male }
+        let female = makeContacts().filter { $0.gender == .female }
+
+        return [male, female]
+    }
+}
+
+
---
+++ UITableView practic/UITableView practic/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  UITableView practic
+//
+//  Created by mac on 01.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+
+    }
+
+
+}
+
