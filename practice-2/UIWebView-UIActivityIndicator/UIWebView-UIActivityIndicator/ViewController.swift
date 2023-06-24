81e8773dd2e9d1fbf76368dffc3607f1d474b284
mangraa1 <georgiu.spare@gmail.com>
2023-06-24 21:48:40 +0300

    initial webview setup

--- practice-2/UIWebView-UIActivityIndicator/UIWebView-UIActivityIndicator/ViewController.swift
+++ practice-2/UIWebView-UIActivityIndicator/UIWebView-UIActivityIndicator/ViewController.swift
@@ -6,14 +6,83 @@
 //
 
 import UIKit
+import WebKit
+import SnapKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    //MARK: - Variables
+
+    let webView = WKWebView()
+    let toolbar = UIToolbar()
+
+    let backButtonItem = UIBarButtonItem(systemItem: .rewind)
+    let fixedSpacer = UIBarButtonItem(systemItem: .fixedSpace)
+    let forwardButtonItem = UIBarButtonItem(systemItem: .fastForward)
+    let flexibleSpacer = UIBarButtonItem(systemItem: .flexibleSpace)
+    let refreshItem = UIBarButtonItem(systemItem: .refresh)
+
+    //MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupWebViewWithToolBar()
+
+        loadRequest()
+
+    }
+
+    //MARK: - private
+
+    private func setupWebViewWithToolBar() {
+        view.addSubview(webView)
+        view.addSubview(toolbar)
+
+        toolbar.barTintColor = .systemBlue
+        toolbar.tintColor = .white
+        toolbar.items = [backButtonItem, fixedSpacer, forwardButtonItem, flexibleSpacer, refreshItem]
+
+        webView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.left.right.equalToSuperview()
+            make.bottom.equalTo(toolbar.snp.top)
+        }
+
+        toolbar.snp.makeConstraints { make in
+            make.left.right.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+
+        backButtonItem.action = #selector(goBackAction)
+        forwardButtonItem.action = #selector(goForwardAction)
+        refreshItem.action = #selector(refreshAction)
+
+    }
+
+    private func loadRequest() {
+        guard let url = URL(string: "https://www.google.com") else { return }
+
+        let urlRequest = URLRequest(url: url)
+        webView.load(urlRequest)
+    }
+
+
+    //MARK: - Actions
+
+    @objc func goBackAction() {
+
+    }
+
+    @objc func goForwardAction() {
+
+    }
+
+    @objc func refreshAction() {
+
     }
 
 
 }
+
 
