920461ff46f0f26c638b6cd9c8f22d13d1b798a5
mangraa1 <georgiu.spare@gmail.com>
2023-06-26 23:55:19 +0300

    implemented Delegate and DataSource

--- practice-2/UIPageViewController/UIPageViewController/Page VC/myPageViewController.swift
+++ practice-2/UIPageViewController/UIPageViewController/Page VC/myPageViewController.swift
@@ -23,7 +23,7 @@ final class myPageViewController: UIPageViewController {
         navigationItem.title = "Cars"
 
         let firstCar = CarsHelper(name: "Bentley Continental", image: continental!)
-        let secondCar = CarsHelper(name: "Rolls-Royce Phantom", image: continental!)
+        let secondCar = CarsHelper(name: "Rolls-Royce Phantom", image: phantom!)
         cars += [firstCar, secondCar]
     }
 
@@ -41,11 +41,51 @@ final class myPageViewController: UIPageViewController {
 
     override init(transitionStyle style: UIPageViewController.TransitionStyle, navigationOrientation: UIPageViewController.NavigationOrientation, options: [UIPageViewController.OptionsKey : Any]? = nil) {
         super.init(transitionStyle: .scroll, navigationOrientation: navigationOrientation)
+
         self.view.backgroundColor = .systemBlue
+
         setViewControllers([arrayCarViewcontroller[0]], direction: .forward, animated: true)
+
+        self.dataSource = self
+        self.delegate = self
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+extension myPageViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewController = viewController as? CarViewController else { return nil }
+
+        if let index = arrayCarViewcontroller.firstIndex(of: viewController) {
+            if index > 0 {
+                return arrayCarViewcontroller[index - 1]
+            }
+        }
+
+        return nil
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewController = viewController as? CarViewController else { return nil }
+
+        if let index = arrayCarViewcontroller.firstIndex(of: viewController) {
+            if index < cars.count - 1 {
+                return arrayCarViewcontroller[index + 1]
+            }
+        }
+
+        return nil
+    }
+
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return cars.count
+    }
+
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+
 }
