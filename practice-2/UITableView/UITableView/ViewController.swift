1075574de2e29a18ff332e45e78dea0db00c2d15
mangraa1 <georgiu.spare@gmail.com>
2023-07-02 11:10:21 +0300

    fixes in TableView

--- practice-2/UITableView/UITableView/ViewController.swift
+++ practice-2/UITableView/UITableView/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
     //MARK: - Custom func
 
     private func setupMyTableView() {
-        myTableView = UITableView(frame: view.bounds, style: .plain)
+        myTableView = UITableView(frame: view.bounds, style: .grouped)
         myTableView.register(UITableViewCell.self, forCellReuseIdentifier: identifire)
         myTableView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
 
@@ -57,7 +57,15 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: identifire, for: indexPath)
 
-        cell.textLabel?.text = "secrion = \(indexPath.section)  row = \(indexPath.row)"
+        cell.textLabel?.text = "section = \(indexPath.section)  row = \(indexPath.row)"
+        cell.accessoryType = .detailButton
+
+        switch indexPath.section {
+        case 0: cell.backgroundColor = .yellow
+        case 1: cell.backgroundColor = .systemPink
+        case 2: cell.backgroundColor = .systemCyan
+        default: break
+        }
 
         return cell
     }
@@ -66,6 +74,19 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80.00
+    }
+
+    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        let alertController = UIAlertController(title: "Alert", message: "section = \(indexPath.section)  row = \(indexPath.row)", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        print(indexPath.row)
     }
 }
 
